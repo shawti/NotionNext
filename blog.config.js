eab49625199d5d01b3b042f5c13cce54479b1bdd
shawti <exiats@gmail.com>
2023-12-07 15:24:55 +0800

    chore: 调整sitemap

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   TITLE: process.env.NEXT_PUBLIC_TITLE || 'OUREA Blog',
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Ourea', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Discover anything about the Generative AI video tools about creative flows, video translation.', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ourea.art', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.ourea.art/blog', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'OUREA,OUREA blog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
